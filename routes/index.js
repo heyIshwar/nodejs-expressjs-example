3f61d157d0e3be5e2abebb5f280718f43fba820f
Ishwar Sarade <ishwar@some1.in>
2020-05-02 15:23:13 +0530

    added /api/searchCountry

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index');
 });
 
 router.get('/form', function (req, res, next) {
   res.render('form');
 });
+
+router.get('/covid', function (req, res, next) {
+  request('https://api.covid19api.com/summary', { json: true }, (err, res2, body) => {
+    if (err) { return console.log(err); }
+    res.render('covid', body);
+  });
+});
+
+router.post('/api/searchCountry', function(req, res, next) {
+  let country = req.body.countryName;
+  request(`https://api.covid19api.com/country/${country}`, { json: true }, (err, res2, body) => {
+    if (err) { return console.log(err); }
+    res.json(body)
+  });
+})
 
 router.post('/postman', function (req, res, next) {
   let email = req.body.email
@@ -19,7 +35,7 @@ router.post('/postman', function (req, res, next) {
     isAuthenticated = true;
   }
 
-  console.log({email, password})
+  console.log({ email, password })
   res.render('postman', {
     recEmail: email,
     recPassword: password,
